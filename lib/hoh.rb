71bb05d625d2b539bf5156bcafa7be3a76b4cb2f
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2019-12-19 23:04:42 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -48,25 +48,25 @@ def naming_system
      label: "Phylum",
      sub_category: {
        label: "Class"
+      }
      }
-   }
- }
+    }
  
- PORTION_2 = {
+   PORTION_2 = {
    label: "Order"
-  }
+    }
  
- PORTION_3 = {
+   PORTION_3 = {
    label: "Family",
    sub_category: {
     label: "Genus",
-   }
- }
+     }
+    }
  
- PORTION_4 = {
+   PORTION_4 = {
    label: "Species",
    sub_category: nil
- }
- }
+      }
+    }
 
 end
